0f77321634dac79d9bcb1dc2062416516db8d354
Rashid <74960869+rashiiddsr@users.noreply.github.com>
2024-03-06 14:35:54 +0700

    Add files via upload

---
+++ Praktikum1/app/src/main/java/com/rashid/helloapps/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rashid.helloapps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
